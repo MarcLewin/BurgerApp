9e99c5ce68db8a0c1db6913ea0b2752fbe173a1d
Your Name <you@example.com>
2018-05-12 21:06:44 -0400

    heroku started

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL){
+connection= mysql.createConnection(process.env.JAWSDB_URL);
+}else{
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "",
+  password: "hinky6699a",
   database: "burgers_db"
 });
-
+};
 
 connection.connect(function(err) {
   if (err) {
